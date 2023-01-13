cc18a07903a44eea93d86df73ba1f5eaae594884
AnaduCG <godzionwin@gmail.com>
2023-01-13 18:12:35 +0100

    Optimized code

--- recharge.py
+++ recharge.py
@@ -1,18 +1,7 @@
 import random
-num1 = random.randrange(0, 10)
-num2 = random.randrange(0, 10)
-num3 = random.randrange(0, 10)
-num4 = random.randrange(0, 10)
-num5 = random.randrange(0, 10)
-num6 = random.randrange(0, 10)
-num7 = random.randrange(0, 10)
-num8 = random.randrange(0, 10)
-num9 = random.randrange(0, 10)
-num10 = random.randrange(0, 10)
-num11 = random.randrange(0, 10)
 
-card = (f"{num1}{num2}{num3}{num4}{num5}{num6}{num7}{num8}{num9}{num10}{num11}")
-amount = random.randrange(100, 1000, 100)
+card = (f"{random.randrange(100_000_000_00)}")
+amount =(f"{ random.randrange(100, 10_000, 100)}")
 code = '*555*'
 complete = (code + card + '#')
 recharge = ''
@@ -26,13 +15,13 @@ while recharge != complete:
         i += 1
         recharge = input(f"Enter Your recharge pin here use {code}pin#: ")
         if recharge == complete:
-            print(f"your rechage of {amount} naira was sucessful")
+            print(f"your recharge of {amount} naira was successful")
             break
         else:
-            print('Recharge unsucessful!')
+            print('Recharge unsuccessful!')
 
         print(f"You only have 5 trials '{5 - i}' remaining")
 
     else:
-        print("Sorry your line has been suspended for 5s")
+        print("Sorry your line has been suspended for an invalid action")
         break
